edd8ec72ef7cd7ba4184e661ff835c6e3ee74d38
clifforc <m.aleksandr@hotmail.com>
2024-09-11 15:40:13 +0300

    add users model and custom user validator

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 
-# Create your models here.
+from validators import validate_username_not_me
+from ..foodgram import constants
+
+class User(AbstractUser):
+    email = models.EmailField(
+        max_length=constants.EMAIL_MAX_LENGTH,
+        unique=True
+    )
+    username = models.CharField(
+        max_length=constants.USERNAME_MAX_LENGTH,
+        unique=True,
+        validators=[UnicodeUsernameValidator(), validate_username_not_me],
+        verbose_name='Имя пользователя'
+    )
+    avatar = models.ImageField(upload_to='users/', null=True, blank='True')
---
+++ backend/users/validators.py
@@ -0,0 +1,11 @@
+from django.core.exceptions import ValidationError
+
+from ..foodgram import constants
+
+
+def validate_username_not_me(value):
+    if value == constants.NOT_ALLOWED_USERNAME:
+        raise ValidationError(
+            {"username": f"Использовать имя '{value}' "
+                         "в качестве username запрещено."}
+        )
