5acacab4c585227ef0ab5ea9ef2b215af1242e34
clifforc <m.aleksandr@hotmail.com>
2024-09-15 16:24:34 +0300

    added user to admin panel fixed app.js to show static pages about and technologies added signup serializer and user serializer

---
+++ backend/api/serializers.py
@@ -0,0 +1,66 @@
+from ctypes.wintypes import tagMSG
+
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+from foodgram import constants
+
+User = get_user_model()
+
+class SignUpSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'first_name', 'last_name', 'password')
+
+        def validate_password(self, value):
+            validate_password(value)
+            return value
+
+        def validate_username(self, value):
+            if value == constants.NOT_ALLOWED_USERNAME:
+                raise serializers.ValidationError(
+                    {"username": f"Использовать имя {value} "
+                                 "в качестве username запрещено."})
+            elif User.objects.filter(username=value).exists():
+                raise serializers.ValidationError(
+                    {"username": "Пользователь "
+                                 f"с именем {value} уже существует"})
+            return value
+
+        def validate_email(self, value):
+            if User.objects.filter(email=value).exists():
+                raise serializers.ValidationError(
+                    {"email": f"Пользователь с адресом {value} уже существует"}
+                )
+            return value
+
+        def create(self, validated_data):
+            user = User.objects.create_user(
+                email=validated_data['email'],
+                username=validated_data['username'],
+                first_name=validated_data['firts_name'],
+                last_name=validated_data['last_name'],
+                password=validated_data['password']
+            )
+            return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    is_subscribed = serializers.BooleanField()
+
+    class Meta:
+        model = User
+        fields = ('email', 'id', 'username', 'first_name', 'last_name',
+                  'is_subscribed', 'avatar')
+
+    def get_is_subscribed(self, obj):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            return obj.subscriber.filter(user=request.user).exists()
+        return False
+
+
+class TokenSerializer(serializers.ModelSerializer):
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = (
+        'email',
+        'username',
+        'is_active',
+        'is_staff',
+        'is_superuser'
+    )
+    search_fields = (
+        'email',
+        'username'
+    )
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        (
+            'Персональная информация', {
+                'fields': ('first_name', 'last_name')
+            }
+        ),
+        ('Разрешения',
+         {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Важные даты', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2',
+                       'is_staff', 'is_active')
+        }),
+    )
+    ordering = ('username',)
