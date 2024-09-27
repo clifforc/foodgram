ee250d0475c86df08d5cc7133713106907960f33
clifforc <m.aleksandr@hotmail.com>
2024-09-27 22:17:51 +0300

    no comments

--- backend/recipes/management/commands/import_CSV.py
+++ backend/recipes/management/commands/import_CSV.py
@@ -3,7 +3,7 @@ import csv
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 
-from backend.recipes.models import Ingredient
+from recipes.models import Ingredient
 
 DIRECTORY = "../data/"
 User = get_user_model()
