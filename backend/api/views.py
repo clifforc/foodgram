fc08dec9908df0e5dbc02ccef9f6c6a169716cc5
clifforc <m.aleksandr@hotmail.com>
2024-09-29 11:11:57 +0300

    env variables fix

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,14 +4,14 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
-from filters import RecipeFilter
-from pagination import CustomPagination
-from permissions import IsAuthorOrReadOnly
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
+from api.filters import RecipeFilter
+from api.pagination import CustomPagination
+from api.permissions import IsAuthorOrReadOnly
 from api.serializers import (
     CustomUserCreateSerializer,
     CustomUserSerializer,
